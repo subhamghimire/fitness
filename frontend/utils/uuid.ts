08a8211aa884323b9f4d363ba1b602ebfb557c27
Shekhar Ghimire <shekhar.ghimire24@gmail.com>
2026-03-21 11:01:22 +0545

    Fix UUI

--- frontend/utils/uuid.ts
+++ frontend/utils/uuid.ts
@@ -1,2 +1,7 @@
-import { v4 as uuidv4 } from 'uuid';
-export function generateId(): string { return uuidv4(); }
+export function generateId(): string {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    const r = (Math.random() * 16) | 0;
+    const v = c === 'x' ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
